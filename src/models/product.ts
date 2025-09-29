a060d4fbd312ce92997aaca6d4d74832b656f22d
Luka Arsov <arsovluka@gmail.com>
2025-09-29 23:43:15 +0200

    Kreirani modeli za proizvod i korisnika

---
+++ src/models/product.ts
@@ -0,0 +1,23 @@
+
+export class Product {
+    id: number;
+    name: string;
+    description: string;
+    amount: number;
+    image: string;
+
+    constructor(id: number, name: string, description: string, amount: number, image: string) {
+        this.id = id;
+
+        this.name = name;
+
+        this.description = description;
+
+        this.amount = amount;
+
+        this.image = image;
+    }
+}
+
+
+
