9e1f061732a2f29c0ce06b188fef72c511ba3d18
Luka Arsov <arsovluka@gmail.com>
2025-10-01 02:16:47 +0200

    update cart.tsx

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,31 +1,30 @@
-import React from 'react'
+import React from "react";
 import { Product } from "../models/product";
-import OneProduct from './OneProduct';
+import OneProduct from "./OneProduct";
 
 interface CartProps {
   allproducts: Product[];
   onAdd: (id: number) => void;
-  onRemove?: ((id: number) => void);
+  onRemove?: (id: number) => void;
 }
 
-const Cart: React.FC<CartProps> = ({ allproducts, onAdd, onRemove}) => {
+const Cart: React.FC<CartProps> = ({ allproducts, onAdd, onRemove }) => {
   return (
     <div className="cart-container">
       <h3>Ovo je vasa korpa.</h3>
-      {allproducts.length === 0? "Nema proizvoda u vasoj korpi" : 
-      allproducts.map((product) => (
-        <OneProduct
-        key={product.id} 
-        product={product}
-        onAdd={() => onAdd(product.id)}
-        onRemove={onRemove ? (id) => onRemove(id) : undefined}
-        inCart={product.amount}
-         />
-      ))}
+      {allproducts.length === 0
+        ? "Nema proizvoda u vasoj korpi"
+        : allproducts.map((product) => (
+            <OneProduct
+              key={product.id}
+              product={product}
+              onAdd={() => onAdd(product.id)}
+              onRemove={onRemove ? (id) => onRemove(id) : undefined}
+              inCart={product.amount}
+            />
+          ))}
     </div>
-  ) }
+  );
+};
 
-
-export default Cart
-
-
+export default Cart;
